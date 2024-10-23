d34a5e1b9ad96784bc69d2799cb83acb99e9cd44
christian-skjetne <chskje@vegvesen.no>
2024-10-23 13:23:38 +0200

    Update citsdecode.py
    Fixed the terrible hexdump

--- citsdecode.py
+++ citsdecode.py
@@ -26,7 +26,7 @@ else:
 if args.input:
     hexGN = args.input
 elif not args.dummy:
-    print("use --help for instructions")
+    print("No input defined, use --help for instructions")
     exit()
 else:
     hexGN = '1200500A038100400380652040018000310A00C9730000000000020337424DAF680F3223B67970066424BC8000000023FD94C00331B18000C800000000000007D2000002012E5BF27181172DF938B2DC124C95D860049CAFED1AAD9A27DC06E7B8380FFFFFFE11DBBA10003C07800304500125000240E530B53E5B23B67970066424BC5A3C8101018003008009707265A61774E81083000000000025CEA28C8400C8010F00030840818002404281060501FFFFFFFF800308408381060501FFFFFFFF80012481040301FFFC80012581050401FFFFFF80018981030201E080018A81030201C080018B8107060130C001FFF880018C81050402FFFFE000018D80013581060501FFFFFFFF80013681060501FFFFFFFF8002027D810201018002027E81050401FFFFFF8002027F81050401FFFFFF808083561CDB2E3B1BA4ABC27EE140DC715544F7A1B15B9C3AFB1618C4A3988FB5187481809F8C60479879F4FC5CC0F56E4FBEA1A42094C2CAB88BFF9E4D35F545A6ED91DF07A56CE11E309280C9B0FE4F382478D45A324642E3C43292C0002964CECA6727808089F79A20EE722E5D6E3684103059D0DF2F5CF4B1488F5DD9E17AB98E6109B428C6F62D54F2193266A5CF877ED4D24C1289B9C9D20C0B8228DB5C9B2C3159C3E3'#+"00"
@@ -81,7 +81,7 @@ hexr = head+IDB+EPB
 #print(hexr)
 bindata = bytearray.fromhex(hexr)#binascii.a2b_hex(hexr)
 #print(bindata)
-bytes = BytesIO(bindata)
+thebytes = BytesIO(bindata)
 #print(bytes)
 
 ## set filenames for -ot hexdump and pcap
@@ -96,49 +96,59 @@ if args.outputtype:
             file_path = os.path.join(os.path.dirname(__file__),args.outputfile)
 
 ## prints a hexdump to the console
-def printHexdump(hexin):
-    hexin = hexin.replace(" ","")
-    word = 0
-    for i in range(0,len(hexin)):
-        if i % 32 == 0:
-            if i == 0:
-                print("0",end='')
-            else:
-                print("\n",end='')
-            print("0",end='')
-            if word < 16:
-                print("0",end='')
-            print(hex(word).lstrip("0x").rstrip("L"),end='')
-            print("0",end='')
-            word = word+1
-            print("  ",end='')
-        if i % 2 == 0:
-            print(" ",end='')
-        print(hexin[i],end='')
-    print()
+def printHexdump(inbytes):
+    for line in hexdump(inbytes):
+        print(line)
 
 ## writes a hexdump to file
-def writeHexdump(hexo):
-    hexo = hexo.replace(" ","")
+def writeHexdump(inbytes):
     with open(file_path, "w") as file:
-        word = 0
-        for i in range(0,len(hexo)):
-            if i % 32 == 0:
-                if i == 0:
-                    file.write("0")
-                else:
-                    file.write("\n")
-                file.write("0")
-                if word < 16:
-                    file.write("0")
-                file.write(hex(word).lstrip("0x").rstrip("L"))
-                file.write("0")
-                word = word+1
-                file.write("  ")
-            if i % 2 == 0:
-                file.write(" ")
-            file.write(hexo[i])
-        file.write("\n")
+        for line in hexdump(inbytes):
+            file.write(line+'\n')
+
+def hexdump(src: bytes, bytesPerLine: int = 16, bytesPerGroup: int = 2, sep: str = '.', ascii: bool = False) -> list:
+    FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
+    lines = []
+    maxAddrLen = len(hex(len(src)))
+    if 8 > maxAddrLen:
+        maxAddrLen = 8
+
+    for addr in range(0, len(src), bytesPerLine):
+        hexString = ""
+        printable = ""
+
+        # The chars we need to process for this line
+        chars = src[addr : addr + bytesPerLine]
+
+        # Create hex string
+        tmp = ''.join(['{:02X}'.format(x) for x in chars])
+        idx = 0
+        for c in tmp:
+            hexString += c
+            idx += 1
+            # 2 hex digits per byte.
+            if idx % bytesPerGroup * 2 == 0 and idx < bytesPerLine * 2:
+                hexString += " "
+        # Pad out the line to fill up the line to take up the right amount of space to line up with a full line.
+        hexString = hexString.ljust(bytesPerLine * 2 + int(bytesPerLine * 2 / bytesPerGroup) - 1)
+
+        # create printable string
+        tmp = ''.join(['{}'.format((x <= 127 and FILTER[x]) or sep) for x in chars])
+        # insert space every bytesPerGroup
+        idx = 0
+        for c in tmp:
+            printable += c
+            idx += 1
+            # Need to check idx because strip() would also delete genuine spaces that are in the data.
+            if idx % bytesPerGroup == 0 and idx < len(chars):
+                printable += " "
+        if(ascii):
+            lines.append(f'{addr:0{maxAddrLen}X}  {hexString}  |{printable}|')
+        else:
+            lines.append(f'{addr:0{maxAddrLen}X}  {hexString}')
+    return lines
+
+
 
 ## writes a binary file
 def writeHexBin(write_byte):
@@ -157,19 +167,19 @@ if args.verbose:
 if args.outputtype:
     if args.outputtype == "hexdump":
         if args.outputfile:
-            writeHexdump(hexr)
+            writeHexdump(bindata)
         else:
-            printHexdump(hexr)
+            printHexdump(bindata)
     elif args.outputtype == 'pcap':
-        writeHexBin(bytes)
+        writeHexBin(thebytes)
     else:
         out = args.outputtype
         runargs.extend(['-r', '-', '-T', out, '-J', filterv])
-        result = subprocess.run(runargs, stdout=subprocess.PIPE, input=bytes.getbuffer())
+        result = subprocess.run(runargs, stdout=subprocess.PIPE, input=thebytes.getbuffer())
         print(result.stdout.decode('utf-8'))
 else:
     runargs.extend(['-r', '-', '-T', 'json', '-J', filterv])
-    result = subprocess.run(runargs, stdout=subprocess.PIPE, input=bytes.getbuffer())
+    result = subprocess.run(runargs, stdout=subprocess.PIPE, input=thebytes.getbuffer())
     print(result.stdout.decode('utf-8'))
 
 
